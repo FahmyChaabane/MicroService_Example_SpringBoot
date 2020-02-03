e94151f58b1fdb48bf7f783820d1dc82b3a50636
FahmyChaabane <chaabane.fahmi@outlook.fr>
2020-02-03 21:18:41 +0100

    second commit in the bourse service

---
+++ bourseService/src/main/java/com/chaabane/project/bourseService/Societe.java
@@ -0,0 +1,42 @@
+package com.chaabane.project.bourseService;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Societe {
+
+    @Id
+    @GeneratedValue
+    private long id;
+    private String nomSociete;
+
+    public Societe(long id, String nomSociete) {
+        this.id = id;
+        this.nomSociete = nomSociete;
+    }
+
+    public Societe(String nomSociete) {
+        this.nomSociete = nomSociete;
+    }
+
+    public Societe() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNomSociete() {
+        return nomSociete;
+    }
+
+    public void setNomSociete(String nomSociete) {
+        this.nomSociete = nomSociete;
+    }
+}
---
+++ bourseService/src/main/java/com/chaabane/project/bourseService/SocieteRepository.java
@@ -0,0 +1,8 @@
+package com.chaabane.project.bourseService;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface SocieteRepository extends JpaRepository<Societe, Long> {
+
+}
