a0e7c7798adc66d8cbd27d22173ed1e505713285
FahmyChaabane <chaabane.fahmi@outlook.fr>
2020-02-03 20:48:59 +0100

    first commit in the bourse service

---
+++ bourseService/src/main/java/com/chaabane/project/bourseService/BourseServiceApplication.java
@@ -0,0 +1,13 @@
+package com.chaabane.project.bourseService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BourseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BourseServiceApplication.class, args);
+	}
+
+}
